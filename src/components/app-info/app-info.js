a14ccd89534526ceaf1af748499f7a51e55c1c8c
maratumerow <maratumerow@gmail.com>
2022-11-05 22:06:50 +0200

    git init

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,10 @@
+
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников в компании N</h1>
+            <h2>Общие число сотрудников</h2>
+        </div>
+    )
+}
