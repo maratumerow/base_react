317b1a6cab8f5491b4126a687a72d41070036318
maratumerow <maratumerow@gmail.com>
2022-11-06 10:34:38 +0200

    create base components

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,10 +1,13 @@
-
+import './app-info.css';
 
 const AppInfo = () => {
     return (
         <div className="app-info">
             <h1>Учет сотрудников в компании N</h1>
             <h2>Общие число сотрудников</h2>
+            <h2>Премию получат: </h2>
         </div>
     )
 }
+
+export default AppInfo;
